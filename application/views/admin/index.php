40a51bfc7eb93ff433dccd05d62b071c9444210d
hafizc8 <hafizc8@gmail.com>
2019-04-03 15:59:02 +0700

    initial_commit

---
+++ application/views/admin/index.php
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+          
+          <div class='panel-body'>
+				<center><img src="<?php echo base_url('assets/img/assian.png');?>" width="720" height="590"></center>
+				
+				
+				</div> <!-- /. panel-body -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      
